3dd0161f7f36140430f27fbafa78960470917fbd
ron <rinbar70@gmail.com>
2024-02-07 11:09:32 +0200

    Add Jade views.

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ import usersRouter from './routes/users';
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
